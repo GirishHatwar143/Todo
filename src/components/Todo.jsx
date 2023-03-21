6ed2aea2cf588aa9c8227f13ca1f597962b80b47
ajayjhaDev <ajaykumarjha05072000@gmail.com>
2023-03-21 16:45:27 +0530

    todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,6 +9,8 @@ const Todo = () => {
   });
 
   const [todo, setTodo] = useState([]);
+  const [isEditMode, setEditMode] = useState(false);
+  const [popdata, setPopdata] = useState({});
 
   const handleChange = (e) => {
     setVal({ ...val, [e.target.name]: e.target.value });
@@ -27,6 +29,43 @@ const Todo = () => {
   const handleDone = (ele) => {
     ele.done = true;
     setTodo([...todo]);
+  };
+
+  const handleDelete = (ele) => {
+    let filteredArr = todo.filter((el) => el.task != ele.task);
+    setTodo([...filteredArr]);
+
+    setVal({
+      task: "",
+      description: "",
+      date: "",
+    });
+  };
+
+  const handlePoPup = (ele) => {
+    setVal({
+      task: ele.task,
+      description: ele.description,
+      date: ele.date,
+      done: false,
+    });
+
+    setEditMode(!isEditMode);
+    setPopdata(ele);
+  };
+
+  const handleEdit = () => {
+    console.log(val);
+    let filteredArr = todo.filter((el) => el.task != popdata.task);
+    setTodo([...filteredArr, val]);
+
+    setEditMode(!isEditMode);
+
+    setVal({
+      task: "",
+      description: "",
+      date: "",
+    });
   };
 
   return (
@@ -55,31 +94,48 @@ const Todo = () => {
           value={val.date}
         />
 
-        <button className="btn btn-primary mt-2" onClick={handleAdd}>
-          Add Todo
-        </button>
+        {isEditMode ? (
+          <button className="btn btn-primary mt-2" onClick={handleEdit}>
+            Edit Todo
+          </button>
+        ) : (
+          <button className="btn btn-primary mt-2" onClick={handleAdd}>
+            Add Todo
+          </button>
+        )}
       </div>
 
-      {todo.map((ele) => (
-        <div
-          key={ele.task}
-          className="text-center mt-3 task"
-          style={
-            ele.done
-              ? { textDecoration: "line-through" }
-              : { textDecoration: "none" }
-          }
-        >
-          <strong>{ele.task}</strong>
-          <input
-            type={"checkbox"}
-            className="ms-4"
-            onClick={() => handleDone(ele)}
-          />
-          <p>{ele.description}</p>
-          <p className="text-warning">{ele.date}</p>
-        </div>
-      ))}
+      <div className="row">
+        {todo.map((ele) => (
+          <div
+            key={ele.task}
+            className="text-center mt-3 task mx-auto col-4"
+            style={
+              ele.done
+                ? { textDecoration: "line-through" }
+                : { textDecoration: "none" }
+            }
+          >
+            <strong>{ele.task}</strong>
+            <input
+              type={"checkbox"}
+              className="ms-4"
+              onClick={() => handleDone(ele)}
+            />
+            <p>{ele.description}</p>
+            <p className="text-warning">{ele.date}</p>
+            <i
+              className="bi bi-trash-fill text-info fs-3"
+              onClick={() => handleDelete(ele)}
+            ></i>
+
+            <i
+              className="bi bi-pencil-square text-info ms-5 fs-3"
+              onClick={() => handlePoPup(ele)}
+            ></i>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
